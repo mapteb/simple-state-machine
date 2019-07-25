17232e9f060c43252dea98fb401a86269af8c0c8
Nalla Senthilnathan <nrsenthil@gmail.com>
2019-07-25 10:29:30 -0400

    PaymentProcessor updated

--- src/main/java/rnd/statemachine/order/PaymentProcessor.java
+++ src/main/java/rnd/statemachine/order/PaymentProcessor.java
@@ -12,6 +12,7 @@ public class PaymentProcessor implements Processor {
     @Override
     public ProcessData process(ProcessData data) throws ProcessException {
         if(((OrderData)data).getPayment() < 1.00) {
+        	((OrderData)data).setEvent(OrderEvent.paymentError);
             throw new PaymentException(OrderEvent.paymentError.name());
         } else {
             ((OrderData)data).setEvent(OrderEvent.paymentSuccess);
