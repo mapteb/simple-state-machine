610f6e4bbab6c8fd1eb8556f905ddd53ef78c9da
Nalla Senthilnathan <nrsenthil@gmail.com>
2019-07-30 10:43:47 -0400

    added test cass

---
+++ src/test/java/rnd/statemachine/order/MockData.java
@@ -0,0 +1,30 @@
+package rnd.statemachine.order;
+
+import java.util.UUID;
+
+class MockData {
+
+    static final UUID orderId = UUID.fromString("cacb4fd3-0139-4402-8ad7-9e8c5aba368a");
+    static final UUID unknownOrderId = UUID.fromString("cacb4fd3-0139-4402-8ad7-9e8c5aba368b");
+    
+    static OrderData SubmitSuccessData() {
+        return OrderData.builder()
+                .orderId(orderId)
+                .event(OrderEvent.orderCreated)
+                .build();
+    }
+    
+    static OrderData paymentSuccessData() {
+        return OrderData.builder()
+                .orderId(orderId)
+                .event(OrderEvent.paymentSuccess)
+                .build();
+    }
+    
+    static OrderData paymentErrorData() {
+        return OrderData.builder()
+                .event(OrderEvent.paymentError)
+                .orderId(orderId)
+                .build();
+    }
+}
---
+++ src/test/java/rnd/statemachine/order/OrderStateTransitionsManagerTest.java
@@ -0,0 +1,69 @@
+package rnd.statemachine.order;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class OrderStateTransitionsManagerTest {
+       
+    @Autowired
+    private ApplicationContext context;
+    
+    @MockBean
+    private OrderProcessor orderProcessor;
+    
+    
+    private OrderDbService dbService = new OrderDbService();
+    
+    @MockBean 
+    PaymentProcessor paymentProcessor;
+    
+    private OrderStateTransitionsManager transitionsManager;
+        
+    @Test
+    public void givenOrderSubmit_whenOrderCreated_thenAssertPaymentPendingState() throws Exception {
+        OrderData data = OrderData.builder().event(OrderEvent.submit).build();
+        
+        transitionsManager = new OrderStateTransitionsManager(context, dbService);
+        when(orderProcessor.process(any())).thenReturn(MockData.SubmitSuccessData());
+        data = (OrderData)transitionsManager.processEvent(data);
+        
+        assertThat(transitionsManager.getStates().get(data.getOrderId())).isEqualTo(OrderState.PaymentPending);
+    } 
+    
+    @Test
+    public void givenOrderPay_whenPaymentEror_thenAssertPaymentPendingState() throws Exception {
+        OrderData data = OrderData.builder()
+                .orderId(MockData.orderId)
+                .event(OrderEvent.pay).payment(0.00).build();
+        dbService.getStates().put(MockData.orderId, OrderState.PaymentPending);
+        transitionsManager = new OrderStateTransitionsManager(context, dbService);
+        when(paymentProcessor.process(any())).thenReturn(MockData.paymentErrorData());
+        data = (OrderData)transitionsManager.processEvent(data);
+        
+        assertThat(transitionsManager.getStates().get(data.getOrderId())).isEqualTo(OrderState.PaymentPending);
+    }
+    
+    @Test
+    public void givenOrderPay_whenPaymentSuccess_thenAssertCreatedState() throws Exception {
+        OrderData data = OrderData.builder()
+                .orderId(MockData.orderId)
+                .event(OrderEvent.pay).payment(1.00).build();
+        
+        dbService.getStates().put(MockData.orderId, OrderState.PaymentPending);
+        transitionsManager = new OrderStateTransitionsManager(context, dbService);
+        
+        when(paymentProcessor.process(any())).thenReturn(MockData.paymentSuccessData());
+        data = (OrderData)transitionsManager.processEvent(data);
+        
+        assertThat(transitionsManager.getStates().get(data.getOrderId())).isEqualTo(OrderState.Completed);
+    }
+}
